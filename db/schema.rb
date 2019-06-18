bcffeca7e9d9829532e92cc98563a6cdd18878cc
zarash <z.ashrafi@yahoo.com>
2019-06-18 10:58:57 +0430

    finish blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_113748) do
+ActiveRecord::Schema.define(version: 2019_06_17_193219) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
